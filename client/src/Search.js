b8f41312548ea8cb2af567304ad6d40e8f016634
NChislom <11775893+nchislom@users.noreply.github.com>
2019-08-01 20:46:24 -0400

    Search and Saved page components started

---
+++ client/src/Search.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./Search.css";
+
+class Search extends Component {
+  render() {
+    return ( 
+    <div class="row">
+        <div class="col s12">
+            <!-- Promo Content 1 goes here -->
+        </div>
+    </div>
+    );
+  }
+}
+
+export default Search;
